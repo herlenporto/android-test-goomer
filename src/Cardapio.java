f5695f673ba6779ee8bf8801a6c6f011b87ba141
Perkles <herlenvital@gmail.com>
2022-06-01 10:05:01 -0300

    criacao cardapio e item

---
+++ src/Cardapio.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+	
+	private List<Item> itens = new ArrayList<>();
+	
+	public void adicionarItem(Item item) {
+		
+	}
+	
+	public void removerItem(Item item) {
+		
+	}
+
+	
+
+}
